acc4664ee9758a2f627d88ed88ef419de2bed267
Carlos <toxz@foxmail.com>
2015-11-26 14:41:19 +0800

    update gradle build

--- src/com/unique/whizzdo/data/DataChangedListener.java
+++
@@ -1,9 +0,0 @@
-package com.unique.whizzdo.data;
-
-/**
- * Created by Carlos on 4/16/2014.
- */
-public interface DataChangedListener {
-    void notifyDataChanged();
-
-}
--- src/com/unique/whizzdo/monitor/FrequencyCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.unique.whizzdo.monitor;
-
-/**
- * Created by carlos on 5/31/14.
- */
-public class FrequencyCalculator {
-
-
-}
