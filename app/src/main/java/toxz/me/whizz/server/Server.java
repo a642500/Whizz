a314c6b43d831a683adafd73eaef16487a202bfc
Carlos <toxz@foxmail.com>
2015-11-28 10:15:04 +0800

    add Hello world http server

--- app/src/main/java/toxz/me/whizz/server/Server.java
+++ app/src/main/java/toxz/me/whizz/server/Server.java
@@ -1,8 +1,30 @@
 package toxz.me.whizz.server;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.koushikdutta.async.http.server.AsyncHttpServer;
+
+import toxz.me.whizz.application.MyBinder;
+
 /**
  * Created by Carlos on 11/28/15.
  */
-public class Server {
+public class Server extends Service {
+    public Server() {
+        AsyncHttpServer server = new AsyncHttpServer();
+        server.get(
+                "/", (request, response) -> {
+                    response.send("Hello World!");
+                });
+        server.listen(4567);
+    }
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new MyBinder(this);
+    }
 }
